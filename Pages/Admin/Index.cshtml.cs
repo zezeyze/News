206e80b89fa2326425a0b836ee1ad442699971ef
zezeyze <minozehra@hotmail.com>
2025-08-01 16:05:33 +0300

    AdminPanel

--- Pages/Admin/Index.cshtml.cs
+++ Pages/Admin/Index.cshtml.cs
@@ -9,16 +9,22 @@ namespace NewsPage.Pages.Admin
     public class IndexModel : PageModel
     {
         private readonly AppDbContext _context;
+        public int ToplamHaber { get; set; }
+        public int YayindaHaber { get; set; }
+        public int TaslakHaber { get; set; }
         public IndexModel(AppDbContext context)
         {
             _context = context;
         }
 
         public IList<Haber> Haberler { get; set; }
-
+       
         public async Task OnGetAsync()
         {
             Haberler = await _context.Haberler.OrderByDescending(h => h.PublishedDate).ToListAsync();
+            ToplamHaber = _context.Haberler.Count();
+            YayindaHaber = _context.Haberler.Count(h => h.IsPublish);
+            TaslakHaber = _context.Haberler.Count(h => !h.IsPublish);
         }
 
         public async Task<IActionResult> OnPostPublishAsync(int id)
@@ -31,5 +37,20 @@ namespace NewsPage.Pages.Admin
 
             return RedirectToPage();
         }
+
+        //Sil iþlemi
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var haber = await _context.Haberler.FindAsync(id);
+            if (haber == null)
+                return NotFound();
+
+            _context.Haberler.Remove(haber);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage();
+        }
+      
     }
 }
+
